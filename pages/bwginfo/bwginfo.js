cb1c566261096747038d1e2f517c6e6fc75c17c5
secondfloor <448322573@qq.com>
2018-12-14 16:41:36 +0800

    内页

--- pages/bwginfo/bwginfo.js
+++ pages/bwginfo/bwginfo.js
@@ -26,7 +26,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+      const aboutDom = wx.createSelectorQuery().selectViewport('#bwgProfileIn');
+      console.log(aboutDom);
   },
 
   /**
